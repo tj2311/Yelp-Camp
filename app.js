d8aaebb9ade378a1ab7585b646c426c56834d6e4
Tejas <tejaspoolu@gmail.com>
2024-07-16 14:58:06 -0400

    changed the database url

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp';
+const dbUrl =  process.env.DB_URL;
 
 
 const campgroundsRoutes = require('./routes/campgrounds');
@@ -26,7 +26,7 @@ const MongoDBStore = require("connect-mongo")(session);
 
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', { // connects to database
+mongoose.connect(dbUrl, { // connects to database
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
